ca32fc2931beb831e54ce7861e52e1ddc409101e
Arzo-chowdhury <arzochowdhury4@gmail.com>
2025-03-14 22:07:39 +0600

    isEven function added

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function calculateSum(a , b){
 
 let result = calculateSum(15, 10);
 console.log(result);
+
+//write a function named isEven that takes one argument and returns true if the number is even , and false if it is not.
+
+function isEven(num){
+    return num % 2 === 0;
+}
+console.log(isEven(4));
+console.log(isEven(9));
