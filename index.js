6c5b9be8af1292032d240367988ce97995fbecb3
Arzo-chowdhury <arzochowdhury4@gmail.com>
2025-03-14 22:01:14 +0600

    calculateSum function added

---
+++ index.js
@@ -0,0 +1,8 @@
+//write a function named calculateSum that takes two arguments and returns the sum of the two arguments.
+
+function calculateSum(a , b){
+    return a + b ;
+}
+
+let result = calculateSum(15, 10);
+console.log(result);
